d00bbd95ba00e10410a1c30a45ed7d06626363be
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-29 13:50:31 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <h2>Hello World</h2>
     </div>
   );
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,32 @@
+import './Header.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className='ms-3' to='/' ><img style={{ height: '40px' }} alt="" /></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        </ul>
+                        <Link className='navbar-button me-5 d-block mb-1' to='/home' >Home</Link>
+                        <Link className='navbar-button me-5 d-block mb-1' to='/addservice' >Add Service</Link>
+                        <Link className='navbar-button me-5 d-block mb-1' to='/youhelped' >You Helped</Link>
+                        <Link className='navbar-button me-5 d-block mb-1' to='/event' >Event</Link>
+                        <Link className='navbar-button me-5 d-block mb-1' to='/blog' >Blog</Link>
+                        <button className='btn btn-primary' >
+                            <Link className='text-white text-decoration-none' to='/register' >Register</Link>
+                        </button>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
