f76d0ecd4e314b67569dbcc5b411bddb3971ca7d
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-29 16:16:05 +0600

    typed js added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,6 +1,7 @@
 import './Banner.css'
 import React from 'react';
 import banner from '../../../Images/Banner/banner.png'
+import Typed from 'react-typed';
 
 const Banner = () => {
     return (
@@ -8,7 +9,16 @@ const Banner = () => {
             <div style={{ backgroundImage: `url(${banner})`, height: '100vh', backgroundSize: 'cover' }} className='d-flex justify-content-center align-items-center' >
                 <div>
                     <h2 className='banner-words'>
-                        Hello World
+                        <Typed
+                            strings={[
+                                'Welcome To Dream Electronics',
+                                'Here You Can Find Lots Of Electronics Device',
+                                'Explore To Find Your Favourite Electronics'
+                            ]}
+                            typeSpeed={40}
+                            backSpeed={50}
+                            loop
+                        />
                     </h2>
                 </div>
             </div>
