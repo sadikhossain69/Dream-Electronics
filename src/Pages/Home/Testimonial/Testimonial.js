f536263998e6d6ec5ecb54fb2929894eab29c39b
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-30 10:17:09 +0600

    react reveal added and set to testimonial section

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -1,6 +1,7 @@
 import './Testimonial.css'
 import React from 'react';
 import testmonial from '../../../Images/Testmonial/testmonial.jpg'
+import { Fade } from 'react-reveal';
 
 const Testimonial = () => {
     return (
@@ -12,14 +13,18 @@ const Testimonial = () => {
                 <div className="row my-5">
                     <div className="col-lg-12 col-md-12 col-sm-12">
                         <div className="row">
-                            <div className="col-lg-6 col-md-12 col-sm-12 text-center">
-                                <img src={testmonial} alt="" />
-                            </div>
-                            <div className="col-lg-6 col-md-12 col-sm-12 testimonial-text">
-                                <h4 className='mt-4'>
-                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it
-                                </h4>
-                            </div>
+                            <Fade left>
+                                <div className="col-lg-6 col-md-12 col-sm-12 text-center">
+                                    <img src={testmonial} alt="" />
+                                </div>
+                            </Fade>
+                            <Fade right>
+                                <div className="col-lg-6 col-md-12 col-sm-12 testimonial-text">
+                                    <h4 className='mt-4'>
+                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it
+                                    </h4>
+                                </div>
+                            </Fade>
                         </div>
                     </div>
                 </div>
