d257be128edf9732eca287816c8d4f3ac88ad33a
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-30 09:45:12 +0600

    testimonial section done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import './Home.css'
 import React from 'react';
 import Banner from '../Banner/Banner';
 import ContactUs from '../ContactUs/ContactUs';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <ContactUs/>
+            <Testimonial/>
         </div>
     );
 };
---
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -0,0 +1,31 @@
+import './Testimonial.css'
+import React from 'react';
+import testmonial from '../../../Images/Testmonial/testmonial.jpg'
+
+const Testimonial = () => {
+    return (
+        <div>
+            <div className='text-center' >
+                <span className='testimonial-header'>Testimonial</span>
+            </div>
+            <div className="container">
+                <div className="row my-5">
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12 col-sm-12 text-center">
+                                <img src={testmonial} alt="" />
+                            </div>
+                            <div className="col-lg-6 col-md-12 col-sm-12 testimonial-text">
+                                <h4 className='mt-4'>
+                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonial;
