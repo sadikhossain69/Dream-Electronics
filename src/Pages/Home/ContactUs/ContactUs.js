abdeea766c16edcb3ccbb6af7f580fb475b0d273
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-01 15:33:43 +0600

    some issue fixed

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -1,5 +1,6 @@
 import './ContactUs.css'
 import React from 'react';
+import map from '../../../Images/Map/map.webp'
 
 const ContactUs = () => {
     return (
@@ -9,7 +10,7 @@ const ContactUs = () => {
             </div>
             <div className="row text-center">
                 <div className='col-lg-6 col-md-12 col-sm-12'>
-                    <img className='img-fluid' src="https://www.howtogeek.com/wp-content/uploads/2021/01/google-maps-satellite.png?height=200p&trim=2,2,2,2" alt="" />
+                    <img className='img-fluid' src={map} alt="" />
                 </div>
                 <div className='col-lg-6 col-md-12 col-sm-12 mt-2'>
                     <input className='contact-input' placeholder='Name' type="text" /> <br />
