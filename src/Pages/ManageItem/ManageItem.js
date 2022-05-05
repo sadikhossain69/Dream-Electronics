c1a721f5de2aac7da6926b0b307cacf8044216a6
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-05 18:00:20 +0600

    manage inventory items added

---
+++ src/Pages/ManageItem/ManageItem.js
@@ -0,0 +1,29 @@
+import './ManageItem.css'
+import React from 'react';
+
+const ManageItem = ({manageItem}) => {
+
+    const { name, description, image, price, quantity, supplier } = manageItem
+
+    return (
+        <div className="col-lg-4 col-md-6 col-sm-12 mt-3">
+            <div className="card card-item" style={{ width: '100%', height: '100%' }}>
+                <img src={image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h6>{name}</h6>
+                    <p className="card-text">{description}</p>
+                    <span className='fw-bold d-block'>Price: ${price}</span>
+                    <span className='fw-bold d-block my-1'>Quantity: {quantity}</span>
+                    <h6 className='fw-bold'>Supplier: {supplier}</h6>
+                    <div className="w-100">
+                        <button className="btn btn-outline-danger w-100">
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ManageItem;
--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -1,22 +1,30 @@
 import './ManageItems.css'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import ManageItem from '../ManageItem/ManageItem';
 
 const ManageItems = () => {
 
     const [manageItems, setManageItems] = useState([])
 
-    useEffect( () => {
+    useEffect(() => {
         const url = `http://localhost:5000/inventories`
         axios.get(url)
-        .then(res => {
-            console.log(res.data);
-        })
-    }, [] )
+            .then(res => {
+                setManageItems(res.data);
+            })
+    }, [manageItems])
 
     return (
-        <div>
-            
+        <div className="container">
+            <div className='row mx-auto' >
+                {
+                    manageItems.map(manageItem => <ManageItem
+                        key={manageItem._id}
+                        manageItem={manageItem}
+                    />)
+                }
+            </div>
         </div>
     );
 };
