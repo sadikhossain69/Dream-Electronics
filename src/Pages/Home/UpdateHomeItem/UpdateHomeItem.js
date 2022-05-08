f6c854cbfa7af3116d02ffff6e26c7c29d3acba7
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-08 19:40:19 +0600

    update items designed successfully

--- src/Pages/Home/UpdateHomeItem/UpdateHomeItem.js
+++ src/Pages/Home/UpdateHomeItem/UpdateHomeItem.js
@@ -1,6 +1,6 @@
 import './UpdateHomeItem.css'
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const UpdateHomeItem = () => {
@@ -19,10 +19,32 @@ const UpdateHomeItem = () => {
         })
     }, [id] )
 
+    const { name, supplier, quantity, price, image } = updateHomeItem
+
     return (
-        <div>
-            <h2>This is update home item!!!</h2>
-            <h2>Name: {updateHomeItem.name}</h2>
+        <div className='container'>
+            <div className="row mx-auto w-50">
+                <span className='text-center my-3'>
+                <Link to='/manageitems' className='btn btn-outline-primary' >Manage Item</Link>
+                </span>
+                <div className='col-lg-12 col-md-12 col-sm-12 update-card text-center'>
+                    <img className='img-fluid' src={image} alt="" />
+                    <h6>Name: {name}</h6>
+                    <h6>Supplier Name: {supplier}</h6>
+                    <p className='fw-bold'>Price: {price}</p>
+                    <p className='fw-bold'>Quantity: {quantity}</p>
+                    <button className='btn btn-primary' >
+                        Deliver
+                    </button>
+                    <input className='d-block mx-auto w-50 my-3' type="number" name="" id="" />
+                    <button className="btn btn-primary mb-3">
+                        Add Quantity
+                    </button>
+                </div>
+                <span className='text-center my-3'>
+                    <Link to='/additems' className='btn btn-outline-success' >Add New Item</Link>
+                </span>
+            </div>
         </div>
     );
 };
