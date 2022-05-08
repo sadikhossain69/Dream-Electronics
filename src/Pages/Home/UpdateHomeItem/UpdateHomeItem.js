a962008f5cf9637bf58a4b2e268ad568fb4fab47
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-08 22:25:16 +0600

    update section done

--- src/Pages/Home/UpdateHomeItem/UpdateHomeItem.js
+++ src/Pages/Home/UpdateHomeItem/UpdateHomeItem.js
@@ -2,22 +2,87 @@ import './UpdateHomeItem.css'
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const UpdateHomeItem = () => {
 
-    const {id} = useParams()
+    const { id } = useParams()
     const [updateHomeItem, setUpdateHomeItem] = useState({})
+    const [input, setInput] = useState(0)
 
-    useEffect( () => {
+    useEffect(() => {
         const url = `http://localhost:5000/inventories/${id}`
         axios.get(url)
-        .then(res => {
-            setUpdateHomeItem(res.data);
-        })
-        .catch(err => {
-            console.error(err)
-        })
-    }, [id] )
+            .then(res => {
+                setUpdateHomeItem(res.data);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [id])
+
+    const handleDeliver = () => {
+        const newQuantity = parseInt(updateHomeItem.quantity) - 1
+        if (newQuantity <= 0) {
+            toast.error("No Stock Here Now")
+        }
+        else {
+            const newUpdateQuantity = { newQuantity }
+
+            const url = `http://localhost:5000/inventories/${id}`
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newUpdateQuantity)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.acknowledged === true) {
+                    setUpdateHomeItem(newUpdateQuantity)
+                    toast("Product Delivery Successfully")
+                }
+                console.log(data);
+            })
+
+        }
+    }
+
+    const handleAddInput = event => {
+        const inputValue = event.target.value
+        setInput(inputValue);
+    }
+
+    const handleAddQuantity = () => {
+        const newQuantity = parseInt(updateHomeItem.quantity) + parseInt(input)
+        console.log(newQuantity);
+        if (newQuantity <= 0) {
+            toast.error("No Stock Here Now")
+        }
+        else {
+            const newUpdateQuantity = { newQuantity }
+
+            const url = `http://localhost:5000/inventories/${id}`
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newUpdateQuantity)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.acknowledged === true) {
+                    setUpdateHomeItem(newUpdateQuantity.newQuantity)
+                    toast("Product Delivery Successfully")
+                }
+                console.log(data);
+            })
+
+        }
+        
+    }
 
     const { name, supplier, quantity, price, image } = updateHomeItem
 
@@ -25,7 +90,7 @@ const UpdateHomeItem = () => {
         <div className='container'>
             <div className="row mx-auto w-50">
                 <span className='text-center my-3'>
-                <Link to='/manageitems' className='btn btn-outline-primary' >Manage Item</Link>
+                    <Link to='/manageitems' className='btn btn-outline-primary' >Manage Item</Link>
                 </span>
                 <div className='col-lg-12 col-md-12 col-sm-12 update-card text-center p-2'>
                     <img className='img-fluid' src={image} alt="" />
@@ -33,11 +98,11 @@ const UpdateHomeItem = () => {
                     <h6>Supplier Name: {supplier}</h6>
                     <p className='fw-bold'>Price: {price}</p>
                     <p className='fw-bold'>Quantity: {quantity}</p>
-                    <button className='btn btn-primary' >
+                    <button onClick={handleDeliver} className='btn btn-primary' >
                         Deliver
                     </button>
-                    <input className='d-block mx-auto w-50 my-3' type="number" name="" id="" />
-                    <button className="btn btn-primary mb-3">
+                    <input onChange={handleAddInput} className='d-block mx-auto w-50 my-3' type="number" name="" id="" />
+                    <button onClick={handleAddQuantity} className="btn btn-primary mb-3">
                         Add Quantity
                     </button>
                 </div>
--- src/Pages/ManageItem/ManageItem.js
+++ src/Pages/ManageItem/ManageItem.js
@@ -1,10 +1,32 @@
 import './ManageItem.css'
 import React from 'react';
 import { Bounce } from 'react-reveal';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const ManageItem = ({ manageItem }) => {
 
-    const { name, description, image, price, quantity, supplier } = manageItem
+    const { _id, name, description, image, price, quantity, supplier } = manageItem
+
+    const handleManageItemDelete = id => {
+        console.log(id);
+        const proceed = window.confirm('Are You Sure?')
+
+        if(proceed) {
+            const url = `http://localhost:5000/addedItems/${id}`
+            axios.delete(url)
+            .then(res => {
+                console.log(res.data);
+                if(res.status === 200) {
+                    toast('Item Deleted, Please Reload The Page')
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            })
+        }
+
+    }
 
     return (
         <>
@@ -18,7 +40,7 @@ const ManageItem = ({ manageItem }) => {
                             <span className='fw-bold d-block'>Price: ${price}</span>
                             <span className='fw-bold d-block my-1'>Quantity: {quantity}</span>
                             <h6 className='fw-bold'>Supplier: {supplier}</h6>
-                            <div className="w-100">
+                            <div onClick={ () => handleManageItemDelete(_id) } className="w-100">
                                 <button className="btn btn-outline-danger w-100">
                                     Delete
                                 </button>
